a33df742f5dd0ba6708470e164f168d76a26b238
sempolenak12 <abrahamgiftri@gmail.com>
2025-01-21 00:08:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -304,7 +304,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/KDxJlW6cxRk?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://drive.google.com/file/d/1COaCV3J4hSakj7WiCDX8tjDabFFU0d1d=0&loop=1&autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
